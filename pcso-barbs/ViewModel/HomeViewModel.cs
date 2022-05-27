72365aea5184c8fc46d4ebd06104aded56905ada
Barbsr08 <broque@pcso.gov.ph>
2022-05-27 13:32:02 +0800

    Implemented SignalR

--- pcso-barbs/ViewModel/HomeViewModel.cs
+++ pcso-barbs/ViewModel/HomeViewModel.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Collections.ObjectModel;
 
-namespace pcso_barbs.ViewModel
+namespace pcso_barbs.ViewModel;
+
+public partial class HomeViewModel : BaseViewModel
 {
-    public partial class HomeViewModel : BaseViewModel
+    [ObservableProperty]
+    public ObservableCollection<string> chatmessages;
+
+    private HubConnection? hubConnection;
+    private ObservableCollection<string> messages = new ObservableCollection<string>();
+    private string? name;
+    private string? message;
+
+    public HomeViewModel()
     {
+        OnInitializedAsync();
+    }
+
+    protected async Task OnInitializedAsync()
+    {
+        hubConnection = new HubConnectionBuilder()
+            .WithUrl("https://pcso-barbs-api.azurewebsites.net/chat")
+
+            .Build();
+
+        hubConnection.On<string, string>("Receive", (name, message) =>
+        {
+            var encodedMsg = $"{name} {message}";
+            messages.Add(encodedMsg);
+            Chatmessages = messages;
+        });
+
+        await hubConnection.StartAsync();
+    }
+
+    private async Task Send()
+    {
+        if (hubConnection is not null)
+        {
+            await hubConnection.SendAsync("Broadcast", name, message);
+            message = null;
+        }
     }
 }
